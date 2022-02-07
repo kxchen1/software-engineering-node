3e42c4f456d1706de3b52419fc6b3727031597c0
chenkexin <chen.kexin@northeastern.edu>
2022-02-06 20:34:58 -0500

    add mongoose

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import express, {Request, Response} from 'express';
 const app = express();
 
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Hello World!'));
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
