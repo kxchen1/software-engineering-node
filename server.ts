f51fa52c37a98a60311a4b84dc4c572a58b211ce
chenkexin <chen.kexin@northeastern.edu>
2022-02-24 14:20:55 -0500

    link to heroku

--- server.ts
+++ server.ts
@@ -5,6 +5,10 @@ import TuitController from "./controllers/TuitController";
 import UserDao from "./daos/UserDao";
 import TuitDao from "./daos/TuitDao";
 import bodyParser from "body-parser";
+import LikeController from "./controllers/LikeController";
+import FollowController from "./controllers/FollowController";
+import BookmarkController from "./controllers/BookmarkController";
+import MessageController from "./controllers/MessageController";
 
 // connect to the database
 
@@ -23,8 +27,11 @@ app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
 
 
-const userController = new UserController(app, new UserDao());
-const tuitController = new TuitController(app, new TuitDao());
-
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+const likeController = LikeController.getInstance(app);
+const followController = FollowController.getInstance(app);
+const bookmarkController = BookmarkController.getInstance(app);
+const messageController = MessageController.getInstance(app);
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
