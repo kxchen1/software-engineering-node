d1ef1e5c019b001c4c27c33b4bcd215a19210284
chenkexin <chen.kexin@northeastern.edu>
2022-03-14 16:19:59 -0400

    a3

--- server.ts
+++ server.ts
@@ -20,15 +20,7 @@ var cors = require('cors')
 
 // build the connection string
 const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
-const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
-// connect to the database
-mongoose.connect(connectionString);
-
+mongoose.connect('mongodb+srv://user1:secretpassword@cluster0.xoijd.mongodb.net/tuiter-a2?retryWrites=true&w=majority');
 const app = express();
 app.use(express.json());
 app.use(cors());
