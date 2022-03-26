edcd8e3bb4e0d7bfa1a65c5e439de7c6c8d7dbc8
chenkexin <chen.kexin@northeastern.edu>
2022-03-25 23:00:49 -0400

    finish the first part

--- controllers/AuthenticationController.ts
+++ controllers/AuthenticationController.ts
@@ -7,7 +7,7 @@ const AuthenticationController = (app: Express) => {
     
     const userDao: UserDao = UserDao.getInstance();
 
-    const login = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response) => {
         const user = req.body;
         const username = user.username;
         const password = user.password;
@@ -51,6 +51,7 @@ const AuthenticationController = (app: Express) => {
         // @ts-ignore
         const profile = req.session['profile'];
         if (profile) {
+            profile.password = "";
             res.json(profile);
         } else {
             res.sendStatus(403);
@@ -63,10 +64,33 @@ const AuthenticationController = (app: Express) => {
         res.sendStatus(200);
     }
 
+
+    const signup = async (req: Request, res: Response) => {
+        const newUser = req.body;
+        const password = newUser.password;
+        const hash = await bcrypt.hash(password, saltRounds);
+        newUser.password = hash;
+
+        const existingUser = await userDao
+            .findUserByUsername(req.body.username);
+        if (existingUser) {
+            res.sendStatus(403);
+            return;
+        } else {
+            const insertedUser = await userDao
+                .createUser(newUser);
+            insertedUser.password = '';
+            // @ts-ignore
+            req.session['profile'] = insertedUser;
+            res.json(insertedUser);
+        }
+    }
+
     app.post("/api/auth/login", login);
     app.post("/api/auth/register", register);
     app.post("/api/auth/profile", profile);
     app.post("/api/auth/logout", logout);
+    app.post("/api/auth/signup", signup);
 }
 
 export default AuthenticationController;
